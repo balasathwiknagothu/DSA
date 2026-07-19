5536a4c208f22e104cdbc852c29abd7d2568e87b
Bala Sathwik Nagothu <balasathwiknagothu@gmail.com>
2026-07-19 21:59:07 +0530

    Time: 0 ms (100%), Space: 44.5 MB (76.82%) - LeetHub

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int camera=0;
+    public int minCameraCover(TreeNode root) {
+        if(dfs(root)==-1)   camera++;
+        return camera;
+    }
+    int dfs(TreeNode root){
+        if(root==null) return 0;
+        int left=dfs(root.left);
+        int right=dfs(root.right);
+        if(left==-1 || right==-1){
+            camera++;
+            return 1;
+        }
+        if(left==1 || right==1){
+            return 0;
+        }
+        return -1;
+    }
+}
