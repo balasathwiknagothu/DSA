d0c13ef9b4aec304a93cb7d7833b9b3a5b378971
Bala Sathwik Nagothu <balasathwiknagothu@gmail.com>
2026-07-27 10:30:20 +0530

    Time: 7 ms (56.12%), Space: 46.8 MB (88.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,25 +1,57 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //DFS
 
 
-        int V=numCourses;
-        List<List<Integer>> adjList=new ArrayList<>();
-        for(int i=0;i<V;i++){
-            adjList.add(new ArrayList<>());
+    //    //DFS
+    //    int V=numCourses;
+    //    List<List<Integer>> adjList=new ArrayList<>();
+    //    for(int i=0;i<V;i++){
+    //        adjList.add(new ArrayList<>());
+    //    }
+    //    for(int[] p: prerequisites){
+    //        adjList.get(p[1]).add(p[0]);
+    //    }
+    //    
+    //    int[] vis=new int[V];
+    //    for(int i=0;i<V;i++){
+    //        if(vis[i]==0){
+    //            if(dfs(i,vis,adjList)==true){
+    //                return false;
+    //            } 
+    //        }
+    //    }
+    //    return true;
+
+
+        //BFS
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
         }
-        for(int[] p: prerequisites){
-            adjList.get(p[1]).add(p[0]);
+        int[] indegree=new int[numCourses];
+        for(int[] p:prerequisites){
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
         }
-        int[] vis=new int[V];
-        for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                if(dfs(i,vis,adjList)==true){
-                    return false;
-                } 
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.offer(i);
             }
         }
-        return true;
+        int count=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            count++;
+            for(int adjNode:adj.get(node)){
+                indegree[adjNode]--;
+                if(indegree[adjNode]==0){
+                    q.offer(adjNode);
+                }
+            }
+        }
+        return count==numCourses;
+
     }
 
     private boolean dfs(int node, int[] vis, List<List<Integer>> adj){
